b6c75234b784ae9990ceb076ffc37ae7d3b7fbb9
Jamie Holdstock <jam.jar69@gmail.com>
2016-09-08 18:50:30 +0100

    delete orders with number >N digits

--- conf.rb
+++ conf.rb
@@ -1,14 +1,18 @@
 require 'inifile'
-	
+
+$file = IniFile.load('conf.ini')
+
 def getAccNameLookup
-	file = IniFile.load('conf.ini')
-	rules = file["Account Name Lookup"]
+	rules = $file["Account Name Lookup"]
 	puts "Loaded #{rules.size} lookup rules from conf.ini"
 	rules
 end
 
 def getPdfName
-	file = IniFile.load('conf.ini')
-	general = file["General Config"]
+	general = $file["General Config"]
 	general["pdfName"]
+end
+
+def getOver6Includes
+	$file["Include even if over 6"]["accountNames"].split ","
 end
--- main.rb
+++ main.rb
@@ -3,16 +3,25 @@ require_relative "pdf-parser"
 
 require "pp"
 
-# if order number is greater than 4 digits, only include if it is ocado
 # if order number is greater than 4 digits and worldstores, create a total row
 # value in spreadsheet should be N value
 # London in town 
+
+def deleteOver6digits data, lll
+		data.each do |date, orders|
+			orders.delete_if do |order|
+				order[:orderNo].length > 4
+			end
+		end
+
+	data
+end
 
 def performNameLookup data, rules
 	replacedNames = 0
 
 	data.each do |date, orders|
-		orders.map do |order|
+		orders.each do |order|
 			rules.each do |k, v|
 				replaced = order[:accountName].gsub! k, v
 				replacedNames += 1 if replaced
@@ -26,7 +35,11 @@ def performNameLookup data, rules
 end
 
 data = getDataFromPdf()
+
 accNameLookup = getAccNameLookup()
 data = performNameLookup(data, accNameLookup)
 
-#pp data
+over6Includes = getOver6Includes()
+data = deleteOver6digits(data, over6Includes)
+
+pp data
